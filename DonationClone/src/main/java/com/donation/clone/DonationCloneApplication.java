12e7d876a019b49e5702877c26e7af5d30534f0d
sangmin <sangmin@DESKTOP-BEO98IK>
2022-12-28 13:57:24 +0900

    프로젝트 초기 등록

---
+++ DonationClone/src/main/java/com/donation/clone/DonationCloneApplication.java
@@ -0,0 +1,13 @@
+package com.donation.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationCloneApplication.class, args);
+	}
+
+}
---
+++ DonationClone/src/test/java/com/donation/clone/DonationCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donation.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
